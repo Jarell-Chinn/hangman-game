e7bc46ac725711f03e4e654788c25b615697d917
kayla <you@example.com>
2023-07-05 14:24:48 -0700

    added pseudo code to HTML and JS

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// define variables and query selectors
+// establish functions
+    //start the game
+        //start timer
+        //present first word data
+    //setIntervals -- timer
+        //if all characters are correct, stop time and increase wins by one
+        //check if reached zero
+            //if timer hits zero unhide all characters
+            //inscrease losses by one
+        //update timer on page
+    //display total wins/losses
+        //local storage
+//event listener(s)
+    //keypress
+    //start game
+    //reset game
